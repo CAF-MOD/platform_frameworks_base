CAF-Extended/frameworks_base

Bringup BaikalOS internals,App profiles and Powesave Settings
Double-line statusbar [1/2]

Change-Id: Iab6e5337d1fdfcce0ccbb712758a531309b21686

fixes

fixes

Add base BaikalOS package

Change-Id: I1aee801b0ac46119a18031e74efd566afb8c50f8

Block significant motion sensor in aggressive or extreme powersave modes

Change-Id: Idb1ab00809f456c680ea47444847581f0bf6cc07

Add core BaikalOS system services

Change-Id: I216e249957774440563fd4801196ac1f41201cf8

fixes

fixes

Bringup BaikalOS internals,App profiles and Powesave Settings

Change-Id: If2c7b9c28d6370110f622e33a7469e7997f54ada

Update internals for BaikalSystemService integration

Change-Id: I1c72bb31429687fd621ad59d6af6c11df29fe615

Introduce BaikalSystemService

Change-Id: Ie5f34d7a9f56682da87f3cd3a8d1aacaf93e7e0e

Baikalify more. Bring new infrastructure.

Change-Id: I0f4e4df8d6e8a14038d976a175abb8b3a99b60b0

Protect baikalos broadcasts

Change-Id: I3bb9d4226333b4b17de7c9c399e55c8fb1de80c9

Add ACTION_PROFILE_CHANGED broadcast

Change-Id: I4bf912c43218c5db00bf67c98205533658f41ac5

import

More baikalos settings

Change-Id: Ib511bdd444bb5520e472b498642d48d31e2853df

Alarm blocker and BaikalStaticService

Change-Id: I2080b1b8b4b9cb5e3de6369ab56e19cad8ca7337

Set GMS uid on start

Change-Id: Ib446602c0aaeaa4e3db2e375dea0738803af5a75

Treat whitelisted apps as persistent.

Change-Id: I1159879a66f16f0565dd15fdbcf52b9eeb35e005

Add BaikalActivityService

Change-Id: If40d7c06d4f05395915b3812b97fa9e15a3106e9

Massive import from Pie

Change-Id: I2b7115ec4f205634e6e942ac0ed061377857b563

Enable aggressive and extreme mode idling

Change-Id: If27d1f4e2c8993c4ca1e587fef419a432981c507

baikal: Add idle modes QS tiles

Change-Id: Ie562d2a171f481c7f66c68c5d1c43a8b18102ef4

Add settings for GMS and Idle

Change-Id: Ide7334c9a03bb0782b10fdb5fe578d95869c9d13

App Profile services

Change-Id: Ic8d2a3662fb36707ec3a26103f878df241704318

Reader Mode

Change-Id: I97a19897de985929490aa76454f66f42f54ebe25

Per app brightness control.

core/java/com/android/internal/baikalos/DevProfileManager.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2019 BaikalOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.baikalos;
+
+import android.util.Slog;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+
+
+public class DevProfileManager extends MessageHandler { 
+
+    private static final String TAG = "Baikal.DevProfle";
+
+    private static Object mLock = new Object();
+
+    @Override
+    protected void initialize() {
+    	synchronized(mLock) {
+    	}
+    	if( Constants.DEBUG_DEV_PROFILE ) Slog.i(TAG,"initialize()");                
+    }
+
+
+    @Override
+    public boolean onMessage(Message msg) {
+    	return false;
+    }
+}

core/java/com/android/internal/baikalos/Torch.java
@@ -1,0 +1,109 @@
+/*
+ * Copyright (C) 2019 BaikalOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.baikalos;
+
+import android.util.Slog;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+
+
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+
+public class Torch extends MessageHandler { 
+
+    private static final String TAG = "Baikal.Torch";
+
+    private static boolean mTorchEnabled = false;
+
+    private static CameraManager mCameraManager;
+    private static String mRearFlashCameraId;
+    private static Object mLock = new Object();
+
+    @Override
+    protected void initialize() {
+	synchronized(mLock) {
+	    mCameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
+	    mCameraManager.registerTorchCallback(new TorchModeCallback(), getHandler());
+	    try {
+	    	mRearFlashCameraId = getRearFlashCameraId();
+	    } catch (CameraAccessException e) {
+	    }
+	}
+	if( Constants.DEBUG_TORCH ) Slog.i(TAG,"initialize()");                
+    }
+
+
+    @Override
+    public boolean onMessage(Message msg) {
+	return false;
+    }
+
+    public static void toggleTorch(boolean on) {
+	synchronized(mLock) {
+	    toggleTorchLocked(on);
+	}
+    }
+
+
+    private static void toggleTorchLocked(boolean on) {
+	if( on !=  mTorchEnabled ) {
+            final boolean origEnabled = mTorchEnabled;
+            try {
+                if (mRearFlashCameraId != null) {
+                    mCameraManager.setTorchMode(mRearFlashCameraId, on);
+                    mTorchEnabled = on;
+                }
+            } catch (CameraAccessException e) {
+            // Ignore
+            }
+        }
+    }
+
+
+    private static String getRearFlashCameraId() throws CameraAccessException {
+        if (mRearFlashCameraId != null) return mRearFlashCameraId;
+        for (final String id : mCameraManager.getCameraIdList()) {
+            CameraCharacteristics c = mCameraManager.getCameraCharacteristics(id);
+            boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+            int lensDirection = c.get(CameraCharacteristics.LENS_FACING);
+            if (flashAvailable && lensDirection == CameraCharacteristics.LENS_FACING_BACK) {
+                mRearFlashCameraId = id;
+            }
+        }
+        return mRearFlashCameraId;
+    }
+    private static class TorchModeCallback extends CameraManager.TorchCallback {
+        @Override
+        public void onTorchModeChanged(String cameraId, boolean enabled) {
+            //if (!cameraId.equals(mRearFlashCameraId)) return;
+            //mTorchEnabled = enabled;
+            //if (!mTorchEnabled) {
+            //    cancelTorchOff();
+            //}
+        }
+        @Override
+        public void onTorchModeUnavailable(String cameraId) {
+            //if (!cameraId.equals(mRearFlashCameraId)) return;
+            //mTorchEnabled = false;
+            //cancelTorchOff();
+        }
+    }
+}

CAF-MOD/platform_frameworks_base

core/java/com/android/internal/baikalos/DevProfileManager.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2019 BaikalOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.baikalos;
+
+import android.util.Slog;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+
+
+public class DevProfileManager extends MessageHandler { 
+
+    private static final String TAG = "Baikal.DevProfle";
+
+    private static Object mLock = new Object();
+
+    @Override
+    protected void initialize() {
+    	synchronized(mLock) {
+    	}
+    	if( BaikalConstants.BAIKAL_DEBUG_DEV_PROFILE ) Slog.i(TAG,"initialize()");                
+    }
+
+
+    @Override
+    public boolean onMessage(Message msg) {
+    	return false;
+    }
+}

core/java/com/android/internal/baikalos/Torch.java
@@ -1,0 +1,109 @@
+/*
+ * Copyright (C) 2019 BaikalOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.baikalos;
+
+import android.util.Slog;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+
+
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+
+public class Torch extends MessageHandler { 
+
+    private static final String TAG = "Baikal.Torch";
+
+    private static boolean mTorchEnabled = false;
+
+    private static CameraManager mCameraManager;
+    private static String mRearFlashCameraId;
+    private static Object mLock = new Object();
+
+    @Override
+    protected void initialize() {
+	synchronized(mLock) {
+	    mCameraManager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
+	    mCameraManager.registerTorchCallback(new TorchModeCallback(), getHandler());
+	    try {
+	    	mRearFlashCameraId = getRearFlashCameraId();
+	    } catch (CameraAccessException e) {
+	    }
+	}
+	if( BaikalConstants.BAIKAL_DEBUG_TORCH ) Slog.i(TAG,"initialize()");                
+    }
+
+
+    @Override
+    public boolean onMessage(Message msg) {
+	return false;
+    }
+
+    public static void toggleTorch(boolean on) {
+	synchronized(mLock) {
+	    toggleTorchLocked(on);
+	}
+    }
+
+
+    private static void toggleTorchLocked(boolean on) {
+	if( on !=  mTorchEnabled ) {
+            final boolean origEnabled = mTorchEnabled;
+            try {
+                if (mRearFlashCameraId != null) {
+                    mCameraManager.setTorchMode(mRearFlashCameraId, on);
+                    mTorchEnabled = on;
+                }
+            } catch (CameraAccessException e) {
+            // Ignore
+            }
+        }
+    }
+
+
+    private static String getRearFlashCameraId() throws CameraAccessException {
+        if (mRearFlashCameraId != null) return mRearFlashCameraId;
+        for (final String id : mCameraManager.getCameraIdList()) {
+            CameraCharacteristics c = mCameraManager.getCameraCharacteristics(id);
+            boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+            int lensDirection = c.get(CameraCharacteristics.LENS_FACING);
+            if (flashAvailable && lensDirection == CameraCharacteristics.LENS_FACING_BACK) {
+                mRearFlashCameraId = id;
+            }
+        }
+        return mRearFlashCameraId;
+    }
+    private static class TorchModeCallback extends CameraManager.TorchCallback {
+        @Override
+        public void onTorchModeChanged(String cameraId, boolean enabled) {
+            //if (!cameraId.equals(mRearFlashCameraId)) return;
+            //mTorchEnabled = enabled;
+            //if (!mTorchEnabled) {
+            //    cancelTorchOff();
+            //}
+        }
+        @Override
+        public void onTorchModeUnavailable(String cameraId) {
+            //if (!cameraId.equals(mRearFlashCameraId)) return;
+            //mTorchEnabled = false;
+            //cancelTorchOff();
+        }
+    }
+}